56703b735e98b0dd9a1bf8aa3e14ce81f1442202
unknown <thomas.zammit@MTDKP116.ix.com>
2014-07-15 14:32:08 +0200

    Hashtag and user now work

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -14,3 +14,29 @@ twitterliteControllers.controller('TwitterCtrl', ['$scope', '$http', function ($
 
 }]);
 
+
+twitterliteControllers.controller('UserCtrl', ['$scope', '$http', '$routeParams',
+function ($scope ,$http,$routeParams) {
+  	
+  	$http.get( 'http://localhost:8080/twitterlite-ws/messages/user?user='+$routeParams.user+'&offset=0&limit=10').success(function( dataCallBack ) {
+		console.log(dataCallBack);
+		 $scope.data = {messages:dataCallBack};
+	 });
+
+  //  console.log($routeParams.user);
+
+}]);
+
+twitterliteControllers.controller('HashTagCtrl', ['$scope', '$http', '$routeParams',
+function ($scope ,$http,$routeParams) {
+  	
+  	$http.get( 'http://localhost:8080/twitterlite-ws/messages/hashtags?hashtags='+$routeParams.hashtag+'&offset=0&limit=10').success(function( dataCallBack ) {
+		console.log(dataCallBack);
+		 $scope.data = {messages:dataCallBack};
+	 });
+
+   // console.log($routeParams.hashtag);
+
+}]);
+
+
