08a248b3ed160c50d7c5991a6fb314b05fb2e692
Chiara Felice Sant Cassia <chiara.felice@ixaris.com>
2014-07-15 15:25:57 +0200

    --

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -4,13 +4,60 @@ var twitterliteControllers = angular.module('twitterliteControllers', []);
 
 twitterliteControllers.controller('TwitterCtrl', ['$scope', '$http', function ($scope ,$http) {
   	
+	$scope.data = [];
+
   	$http.get( 'http://localhost:8080/twitterlite-ws/messages?offset=0&limit=22').success(function( dataCallBack ) {
 		console.log(dataCallBack);
-		 $scope.data = {messages:dataCallBack};
-	 });
+		$scope.data = {messages:dataCallBack};
 
-    $scope.data = 'hello chiara';
-    console.log('hello');
+		// var tagslistarr = $scope.data[i].content.split(' ');
+		// var arr=[];
+
+		// $scope.tweet = '';
+
+		// for(var i = 0; i < tagslistarr.length; i++) {
+		//     if(tagslistarr[i].indexOf('#') == 0){
+		//     	var tag = tagslistarr[i].substring(1);
+		//     	var url = "content.html?hashtag=%22"+tag+"%22";
+		    	
+		//     	$scope.tweet += "<a href=" + url + ">" + tagslistarr[i] + " </a>";
+		        
+		//     } else if(tagslistarr[i].indexOf('@') == 0){
+		//     	var user = tagslistarr[i].substring(1);
+
+		//     	var url = "content.html?user=%22"+user+"%22";
+		//     	$scope.tweet += "<a href=" + url + ">" + tagslistarr[i] + " </a>";
+
+		//     } else {
+		//     	$scope.tweet += tagslistarr[i] + " ";
+		//     }
+		// }
+	});
+
+
+
 
 }]);
 
+		// var tagslistarr = $scope.data[i].content.split(' ');
+		// var arr=[];
+
+		// $scope.tweet = '';
+
+		// for(var i = 0; i < tagslistarr.length; i++) {
+		//     if(tagslistarr[i].indexOf('#') == 0){
+		//     	var tag = tagslistarr[i].substring(1);
+		//     	var url = "content.html?hashtag=%22"+tag+"%22";
+		    	
+		//     	$scope.tweet += "<a href=" + url + ">" + tagslistarr[i] + " </a>";
+		        
+		//     } else if(tagslistarr[i].indexOf('@') == 0){
+		//     	var user = tagslistarr[i].substring(1);
+
+		//     	var url = "content.html?user=%22"+user+"%22";
+		//     	$scope.tweet += "<a href=" + url + ">" + tagslistarr[i] + " </a>";
+
+		//     } else {
+		//     	$scope.tweet += tagslistarr[i] + " ";
+		//     }
+		// }
