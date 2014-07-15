3efce7220230ce0b7878947787d47c29ddf833a9
Chiara Felice Sant Cassia <chiara.felice@ixaris.com>
2014-07-15 16:44:24 +0200

    Fixed tags on usernames and hashtags in every view: todo - isolate the method

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -12,7 +12,6 @@ twitterliteControllers.controller('TwitterCtrl', ['$scope', '$http', function ($
 		
 		console.log(dataCallBack.length);
 
-
 		for (var j = 0; j < dataCallBack.length; j++) { 		
 
 			var tagslistarr = dataCallBack[j].content.split(' ');
@@ -23,19 +22,19 @@ twitterliteControllers.controller('TwitterCtrl', ['$scope', '$http', function ($
 			for(var i = 0; i < tagslistarr.length; i++) {
 			    if(tagslistarr[i].indexOf('#') == 0){
 			    	var tag = tagslistarr[i].substring(1);
-			    	var url = 'hashtag/' + tag ;
-			    	$scope.tweet += "<a href=" + url + ">'" + tagslistarr[i] + "'</a>";	        
+			    	var url = '#hashtag/' + tag ;
+			    	$scope.tweet += '<a href=' + url + '> ' + tagslistarr[i] + ' </a>';	        
 			    } else if(tagslistarr[i].indexOf('@') == 0){
 			    	var user = tagslistarr[i].substring(1);
-			    	var url = 'user/' +user ;
-			    	$scope.tweet += "<a href=" + url + ">'" + tagslistarr[i] + "'</a>";	        
+			    	var url = '#user/' +user ;
+			    	$scope.tweet += '<a href=' + url + '> ' + tagslistarr[i] + ' </a>';	        
 			    } else {
 			    	$scope.tweet += tagslistarr[i] + ' ';
 			    }
 			}
 
 			$scope.data[j] = {
-				    username: dataCallBack[j].user,
+				    username: dataCallBack[j].username,
 				    content: $scope.tweet
 			};
 		}
@@ -52,18 +51,88 @@ twitterliteControllers.controller('TwitterCtrl', ['$scope', '$http', function ($
 twitterliteControllers.controller('UserCtrl', ['$scope', '$http', '$routeParams',
 function ($scope ,$http,$routeParams) {
   	
+  	$scope.data = [];
+
   	$http.get( 'http://localhost:8080/twitterlite-ws/messages/user?user='+$routeParams.user+'&offset=0&limit=10').success(function( dataCallBack ) {
 		console.log(dataCallBack);
-		 $scope.data = {messages:dataCallBack};
+		// $scope.data = {messages:dataCallBack};
+		
+		console.log(dataCallBack.length);
+
+		for (var j = 0; j < dataCallBack.length; j++) { 		
+
+			var tagslistarr = dataCallBack[j].content.split(' ');
+			var arr=[];
+
+			$scope.tweet = '';
+
+			for(var i = 0; i < tagslistarr.length; i++) {
+			    if(tagslistarr[i].indexOf('#') == 0){
+			    	var tag = tagslistarr[i].substring(1);
+			    	var url = '#hashtag/' + tag ;
+			    	$scope.tweet += '<a href=' + url + '> ' + tagslistarr[i] + ' </a>';	        
+			    } else if(tagslistarr[i].indexOf('@') == 0){
+			    	var user = tagslistarr[i].substring(1);
+			    	var url = '#user/' +user ;
+			    	$scope.tweet += '<a href=' + url + '> ' + tagslistarr[i] + ' </a>';	        
+			    } else {
+			    	$scope.tweet += tagslistarr[i] + ' ';
+			    }
+			}
+
+			$scope.data[j] = {
+				    username: dataCallBack[j].username,
+				    content: $scope.tweet
+			};
+		}
+
+		console.log($scope.data);
+
 	 });
 }]);
+
+
+
 
 twitterliteControllers.controller('HashTagCtrl', ['$scope', '$http', '$routeParams',
 function ($scope ,$http,$routeParams) {
   	
+  	$scope.data = [];
+
   	$http.get( 'http://localhost:8080/twitterlite-ws/messages/hashtags?hashtags='+$routeParams.hashtag+'&offset=0&limit=10').success(function( dataCallBack ) {
 		console.log(dataCallBack);
-		 $scope.data = {messages:dataCallBack};
+		// $scope.data = {messages:dataCallBack};
+		
+		console.log(dataCallBack.length);
+
+		for (var j = 0; j < dataCallBack.length; j++) { 		
+
+			var tagslistarr = dataCallBack[j].content.split(' ');
+			var arr=[];
+
+			$scope.tweet = '';
+
+			for(var i = 0; i < tagslistarr.length; i++) {
+			    if(tagslistarr[i].indexOf('#') == 0){
+			    	var tag = tagslistarr[i].substring(1);
+			    	var url = '#hashtag/' + tag ;
+			    	$scope.tweet += '<a href=' + url + '> ' + tagslistarr[i] + ' </a>';	        
+			    } else if(tagslistarr[i].indexOf('@') == 0){
+			    	var user = tagslistarr[i].substring(1);
+			    	var url = '#user/' +user ;
+			    	$scope.tweet += '<a href=' + url + '> ' + tagslistarr[i] + ' </a>';	        
+			    } else {
+			    	$scope.tweet += tagslistarr[i] + ' ';
+			    }
+			}
+
+			$scope.data[j] = {
+				    username: dataCallBack[j].username,
+				    content: $scope.tweet
+			};
+		}
+
+		console.log($scope.data);
 	 });
 }]);
 
